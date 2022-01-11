5d55cb9e5d2a261619b319e96e6d7255ad9bb7ba
henrique <henrique.lommes@gmail.com>
2022-01-11 11:15:53 -0300

    Apicaçao Console

---
+++ DIO/Pilha.cs
@@ -0,0 +1,46 @@
+namespace Pilhass
+{
+
+    public class Pilha
+    {
+        Posicao primeiro ;
+
+        public void Empilha(object item)
+        {
+            primeiro = new Posicao(primeiro, item);
+        }
+
+        public object Desempilha()
+        {
+            if (primeiro == null)
+            {
+                throw new InvalidOperationException("Erro ao DESEMPILHAR!! ");
+            }
+
+            object resultado = primeiro.item;
+            primeiro = primeiro.proximo;
+
+            return resultado;
+        }
+
+        class Posicao
+        {
+            public Posicao proximo;
+
+            public object item;
+
+            public Posicao(Posicao proximo, object item)
+            {
+
+                this.proximo = proximo;
+                this.item = item;
+            }
+        }
+
+    }
+
+
+
+
+
+}
---
+++ DIO/Program.cs
@@ -0,0 +1,25 @@
+﻿using Pilhass;
+
+namespace Programs
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Pilha chaps = new Pilha();
+
+            chaps.Empilha(1);
+            chaps.Empilha(2);
+            chaps.Empilha(3);
+            chaps.Empilha(4);
+            Console.WriteLine(chaps.Desempilha());
+            Console.WriteLine(chaps.Desempilha());
+            Console.WriteLine(chaps.Desempilha());
+            Console.WriteLine(chaps.Desempilha());
+
+
+        }
+
+    }
+}
